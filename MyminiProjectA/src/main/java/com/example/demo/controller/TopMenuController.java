fe06451e2b6ac1b08f93743e9ed7653b29b644ed
HyeonWoong <gusdnd3150@gmail.com>
2021-02-21 15:45:10 +0900

    백업

--- MyminiProjectA/src/main/java/com/example/demo/controller/TopMenuController.java
+++ MyminiProjectA/src/main/java/com/example/demo/controller/TopMenuController.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.TopMenuService;
+import com.example.demo.vo.PagingVo;
 import com.example.demo.vo.UserVo;
 
 @Controller
@@ -22,9 +25,14 @@ public class TopMenuController {
 	
 	//인재 리스트
 	@RequestMapping("/people.do")
-	public String peopleList(HttpServletRequest request,Model model) {
+	public String peopleList(HttpServletRequest request,Model model,
+			@ModelAttribute("searchForm") PagingVo paging,
+		 @RequestParam(value="pageNo", required=false, defaultValue="1") int pageNo
+			) {
 		UserVo user = (UserVo) request.getSession().getAttribute("USER");
 		model.addAttribute("userDetail", user);
+		
+		//PagingVo paging = new PagingVo();
 		
 		List<UserVo> peopleList =service.selectUserList();
 		
